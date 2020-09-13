f28b143f8c96bd5d603074f771c1f6b62c67319a
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2020-09-13 18:40:54 -0500

    Refactor: change if validation statements fields for a checkRequired function

--- form-validator/js/script.js
+++ form-validator/js/script.js
@@ -2,13 +2,29 @@ const form = document.getElementById('form');
 const username = document.getElementById('username');
 const email = document.getElementById('email');
 const passwd = document.getElementById('password');
-const passwdConfirm = document.getElementById('password-confirm');
+const passwdConfirm = document.getElementById('password-confirmation');
+
+function makeCapital(input) {
+  const text = input.id.charAt(0).toUpperCase() + input.id.slice(1);
+  return text.replace(/-/g, ' ');
+}
 
 // Email Regex from
 // https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
 function isValidEmail(email) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
+}
+
+// Check required fields
+function checkRequired(inputArr) {
+  inputArr.forEach(input => {
+    if (input.value.trim() === '') {
+      showError(input, `${makeCapital(input)} is required!`);
+    } else {
+      showSuccess(input);
+    }
+  });
 }
 
 // Show input error message
@@ -29,29 +45,5 @@ function showSuccess(input) {
 form.addEventListener('submit', e => {
   e.preventDefault();
 
-  if (username.value === '') {
-    showError(username, 'Username is required!');
-  } else {
-    showSuccess(username);
-  }
-
-  if (email.value === '') {
-    showError(email, 'Email is required!');
-  } else if (!isValidEmail(email.value)) {
-    showError(email, 'Email is not valid!');
-  } else {
-    showSuccess(email);
-  }
-
-  if (passwd.value === '') {
-    showError(passwd, 'Password is required!');
-  } else {
-    showSuccess(passwd);
-  }
-
-  if (passwdConfirm.value === '') {
-    showError(passwdConfirm, 'Password comfirmation is required!');
-  } else {
-    showSuccess(passwdConfirm);
-  }
+  checkRequired([username, email, passwd, passwdConfirm]);
 });
