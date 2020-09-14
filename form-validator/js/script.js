933f05c14def03c8d5a7fdd59e7719a78da44fec
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2020-09-14 13:56:21 -0500

    Refactor: JS file delete, makeCapital, change checkEmail and add checkPasswdsMatch functions

--- form-validator/js/script.js
+++ form-validator/js/script.js
@@ -4,46 +4,76 @@ const email = document.getElementById('email');
 const passwd = document.getElementById('password');
 const passwdConfirm = document.getElementById('password-confirmation');
 
-function makeCapital(input) {
-  const text = input.id.charAt(0).toUpperCase() + input.id.slice(1);
-  return text.replace(/-/g, ' ');
-}
-
 // Email Regex from
 // https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
-function isValidEmail(email) {
+function checkEmail(input) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
+
+  if (re.test(input.value.trim()))
+    showSuccess(input);
+  else
+    showError(input, 'Email is not valid');
+}
+
+function checkLength(input, min, max) {
+  if (input.value.length < min) {
+    showError(
+      input,
+      `Must be between ${min} and ${max} long!`
+    );
+  } else {
+    showSuccess(input);
+  }
+}
+
+function checkPasswdsMatch(passwd, passwdConfirm) {
+  if (passwd.value !== passwdConfirm.value)
+    showError(passwdConfirm, 'Passwords do not match!');
 }
 
 // Check required fields
 function checkRequired(inputArr) {
   inputArr.forEach(input => {
     if (input.value.trim() === '') {
-      showError(input, `${makeCapital(input)} is required!`);
+      showError(input, 'is required!');
     } else {
       showSuccess(input);
     }
   });
 }
 
+// Hide error message when you time
+function hideErrorMsg(input) {
+  const formCtrl = input.parentElement;
+  const msg = formCtrl.querySelector('.form__msg');
+  input.className = 'form__input';
+  msg.className = 'form__msg';
+}
+
 // Show input error message
 function showError(input, message) {
   const formCtrl = input.parentElement;
-  const small = formCtrl.querySelector('small');
+  const msg = formCtrl.querySelector('.form__msg');
 
-  formCtrl.className = 'form-control error';
-  small.innerText = message;
+  input.className = 'form__input form__input--error';
+  msg.className = 'form__msg form__msg--show';
+  msg.innerText = message;
 }
 
 // Show success outline
 function showSuccess(input) {
   const formCtrl = input.parentElement;
-  formCtrl.className = 'form-control success';
+  const msg = formCtrl.querySelector('.form__msg');
+  input.className = 'form__input form__input--success';
+  msg.className = 'form__msg';
 }
 
 form.addEventListener('submit', e => {
   e.preventDefault();
 
   checkRequired([username, email, passwd, passwdConfirm]);
+  checkEmail(email);
+  checkLength(username, 3, 16);
+  checkLength(password, 6, 16);
+  checkPasswdsMatch(passwd, passwdConfirm);
 });
